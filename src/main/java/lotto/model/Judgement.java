addc5bd297a3b218d41c2547c2f8e50050dcd564
jungyunmok <jungyunmok@gmail.com>
2023-12-12 23:30:10 +0900

    feat: 번호 비교 카운트 기능

--- src/main/java/lotto/model/Judgement.java
+++ src/main/java/lotto/model/Judgement.java
@@ -1,5 +1,8 @@
 package lotto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Judgement {
     public int checkInt(String strNumber) {
         int number;
@@ -11,4 +14,17 @@ public class Judgement {
         return number;
     }
 
+    public double checkLotto(List<Integer> userLotto, int userBonus, List<Integer> winLotto, int winBonus) {
+        List<Integer> rankCount = new ArrayList<>();
+        double correctCount = 0.0;
+        for(int number : userLotto) {
+            if(winLotto.contains(number)) {
+                correctCount++;
+            }
+        }
+        if(winBonus == userBonus) {
+            correctCount += 0.5;
+        }
+        return correctCount;
+    }
 }
