fd689f4448a01f45c0fed59200e3e9bd20a74003
jungyunmok <jungyunmok@gmail.com>
2023-12-12 23:46:13 +0900

    feat: 당첨 기준 Prize enum 추가

--- src/main/java/lotto/constant/Prize.java
+++ src/main/java/lotto/constant/Prize.java
@@ -1,4 +1,37 @@
 package lotto.constant;
 
 public enum Prize {
+    FIRST(6, "1등", "6개 일치", 2000000000),
+    SECOND(5.5, "2등", "6개 일치", 30000000),
+    THIRD(5, "3등", "5개 일치, 보너스 볼 일치", 1500000),
+    FOURTH(4, "4등", "4개 일치", 50000),
+    FIFTH(3, "5등", "3개 일치", 5000);
+
+    private final double correctCount;
+    private final String rank;
+    private final String description;
+    private final int money;
+
+    Prize(double correctCount, String rank, String description, int money) {
+        this.correctCount = correctCount;
+        this.rank = rank;
+        this.description = description;
+        this.money = money;
+    }
+
+    public double getCorrectCount() {
+        return correctCount;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getMoney() {
+        return money;
+    }
 }
--- src/main/java/lotto/model/Judgement.java
+++ src/main/java/lotto/model/Judgement.java
@@ -15,14 +15,13 @@ public class Judgement {
     }
 
     public double checkLotto(List<Integer> userLotto, int userBonus, List<Integer> winLotto, int winBonus) {
-        List<Integer> rankCount = new ArrayList<>();
         double correctCount = 0.0;
-        for(int number : userLotto) {
-            if(winLotto.contains(number)) {
+        for (int number : userLotto) {
+            if (winLotto.contains(number)) {
                 correctCount++;
             }
         }
-        if(winBonus == userBonus) {
+        if (winBonus == userBonus) {
             correctCount += 0.5;
         }
         return correctCount;
