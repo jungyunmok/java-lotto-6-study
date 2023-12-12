8ac3f30e9b04f9c4618ed9c09a4fcfb6ad52c916
jungyunmok <jungyunmok@gmail.com>
2023-12-12 23:16:55 +0900

    feat: 문자열 인트 변환 기능

--- src/main/java/lotto/model/Judgement.java
+++ src/main/java/lotto/model/Judgement.java
@@ -1,4 +1,14 @@
 package lotto.model;
 
 public class Judgement {
+    public int checkInt(String strNumber) {
+        int number;
+        try {
+            number = Integer.parseInt(strNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(e);
+        }
+        return number;
+    }
+
 }
