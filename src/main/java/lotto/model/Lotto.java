c2b84e1e4b47a162f9109102aa8a40a57d5f5e29
jungyunmok <jungyunmok@gmail.com>
2023-12-13 17:02:42 +0900

    refactor: 로또 중복, 길이 조건 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -18,10 +18,15 @@ public class Lotto {
         return numbers;
     }
 
-    // 6자리인지 길이 검사
+    // 중복없는 숫자 6개인지 검사
     private void validate(List<Integer> numbers) {
         if (numbers.size() != LottoInfo.TOTAL_LENGTH) {
             throw new IllegalArgumentException();
+        }
+        for(int number : numbers) {
+            if(Collections.frequency(numbers, number) > 1) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 }
