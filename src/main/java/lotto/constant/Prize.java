4bda46e8b731fea2ab87f9c3c92f554d648983fa
jungyunmok <jungyunmok@gmail.com>
2023-12-13 13:56:53 +0900

    feat: 당첨 내역, 수익률 출력 기능

--- src/main/java/lotto/constant/Prize.java
+++ src/main/java/lotto/constant/Prize.java
@@ -1,37 +1,43 @@
 package lotto.constant;
 
 public enum Prize {
-    FIRST(6.0, "1등", "6개 일치", 2000000000),
-    SECOND(5.5, "2등", "6개 일치", 30000000),
-    THIRD(5.0, "3등", "5개 일치, 보너스 볼 일치", 1500000),
-    FOURTH(4.0, "4등", "4개 일치", 50000),
-    FIFTH(3.0, "5등", "3개 일치", 5000);
+    FIRST(4, 6.0, "1등", "6개 일치", 2000000000),
+    SECOND(3, 5.5, "2등", "5개 일치, 보너스 볼 일치", 30000000),
+    THIRD(2, 5.0, "3등", "5개 일치", 1500000),
+    FOURTH(1, 4.0, "4등", "4개 일치", 50000),
+    FIFTH(0, 3.0, "5등", "3개 일치", 5000);
 
-    private final double correctCount;
-    private final String rank;
-    private final String description;
-    private final int money;
+    private final int INDEX;
+    private final double CORRECT_COUNT;
+    private final String RANK;
+    private final String DESCRIPTION;
+    private final int MONEY;
 
-    Prize(double correctCount, String rank, String description, int money) {
-        this.correctCount = correctCount;
-        this.rank = rank;
-        this.description = description;
-        this.money = money;
+    Prize(int index, double correctCount, String rank, String description, int money) {
+        this.INDEX = index;
+        this.CORRECT_COUNT = correctCount;
+        this.RANK = rank;
+        this.DESCRIPTION = description;
+        this.MONEY = money;
+    }
+
+    public int getINDEX() {
+        return INDEX;
     }
 
     public double getCorrectCount() {
-        return correctCount;
+        return CORRECT_COUNT;
     }
 
     public String getRank() {
-        return rank;
+        return RANK;
     }
 
     public String getDescription() {
-        return description;
+        return DESCRIPTION;
     }
 
     public int getMoney() {
-        return money;
+        return MONEY;
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,11 @@
 package lotto.view;
 
+import lotto.constant.Prize;
 import lotto.model.Lotto;
 
+import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     public void printLotto(List<Lotto> lottoGames) {
@@ -13,4 +16,22 @@ public class OutputView {
         }
     }
 
+    public void printPrize(Map<Double, Integer> rankCount, int i) {
+        DecimalFormat df = new DecimalFormat("###,###");
+        for (Prize prize : Prize.values()) {
+            if (prize.getINDEX() == i) {
+                System.out.println(prize.getDescription() + " (" + df.format(prize.getMoney()) + "원) - " + rankCount.get(prize.getCorrectCount()) + "개");
+            }
+        }
+    }
+
+    public void lottoResult(Map<Double, Integer> rankCount, double rate) {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (int i = 0; i < 5; i++) {
+            printPrize(rankCount, i);
+        }
+        System.out.println("총 수익률은 " + String.format("%.1f", rate) + "입니다.");
+    }
 }
